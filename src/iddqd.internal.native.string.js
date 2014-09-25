459a28b7fe098e43d4e0cd09502b5b8a24e3e7b5
Ron Valstar <sjeiti@gmail.com>
2014-09-25 18:45:30 +0200

    added iddqd.internal.native.string.toSlug

--- src/iddqd.internal.native.string.js
+++ src/iddqd.internal.native.string.js
@@ -169,5 +169,22 @@ iddqd.ns('iddqd.internal.native.string',(function(internal){
 			}
 			return sHash;
 		}
+		,toSlug: function(str) {
+			str = str.replace(/^\s+|\s+$/g,''); // trim
+			str = str.toLowerCase();
+
+			// remove accents, swap ñ for n, etc
+			var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+			var to = "aaaaeeeeiiiioooouuuunc------";
+			for (var i = 0, l = from.length; i<l; i++) {
+				str = str.replace(new RegExp(from.charAt(i),'g'),to.charAt(i));
+			}
+
+			str = str.replace(/[^a-z0-9 -]/g,'') // remove invalid chars
+				.replace(/\s+/g,'-') // collapse whitespace and replace by -
+				.replace(/-+/g,'-'); // collapse dashes
+
+			return str;
+		}
 	});
 })(iddqd.internal));
